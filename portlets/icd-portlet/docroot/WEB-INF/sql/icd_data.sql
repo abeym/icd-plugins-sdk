5f813458a8f3751030c01f7bcd2dcd09b1983f91
abeym <abey_m@yahoo.com>
2013-05-28 15:15:09 -0400

    adding data sql

---
+++ portlets/icd-portlet/docroot/WEB-INF/sql/icd_data.sql
@@ -0,0 +1,19 @@
+INSERT INTO `icd_references`
+(`referencesId`,`groupId`,`companyId`,`userId`,`userName`,`referencesName`,`key_`,`value`,`active_`,`order_`)
+VALUES
+(1,1,1,1,'test','healthplans','healthplan1','health plan 1',1,1);
+
+INSERT INTO `icd_references`
+(`referencesId`,`groupId`,`companyId`,`userId`,`userName`,`referencesName`,`key_`,`value`,`active_`,`order_`)
+VALUES
+(2,1,1,1,'test','clearinghouses','clearinghouse1','clearing house 1',1,1);
+
+INSERT INTO `icd_references`
+(`referencesId`,`groupId`,`companyId`,`userId`,`userName`,`referencesName`,`key_`,`value`,`active_`,`order_`)
+VALUES
+(3,1,1,1,'test','ehrAndBillingVendors','billingVendor1','billing vendor 1',1,1);
+
+INSERT INTO `icd_references`
+(`referencesId`,`groupId`,`companyId`,`userId`,`userName`,`referencesName`,`key_`,`value`,`active_`,`order_`)
+VALUES
+(4,1,1,1,'test','revCycleVendors','revCycleVendor1','revcycle vendor 1',1,1);
